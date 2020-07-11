d3becbd302c9a8ae5692f4e0dcea25fac10d295c
ddyasmile <1271569621@qq.com>
2020-07-11 18:30:15 +0800

    [add] bili subscribe (with bugs)

--- hoshino/modules/bilisubscribe/__init__.py
+++ hoshino/modules/bilisubscribe/__init__.py
@@ -1,4 +1,5 @@
 import asyncio
+import copy
 
 from nonebot import MessageSegment
 
@@ -23,12 +24,13 @@ async def switch_subscribe(bot, ev: CQEvent, sub: bool):
     else:
         ss.add_subscription(mid, ev.group_id) if sub else ss.del_subscription(mid, ev.group_id)
         sv.logger.info(f'群{ev.group_id}{action_tip}了UP主{mid}')
-        await bot.send(ev, f'已为群{ev.group_id}{action_tip}了UP主{mid}'， at_sender=True)
+        await bot.send(ev, f'已为群{ev.group_id}{action_tip}了UP主{mid}', at_sender=True)
 
 @sv.on_prefix(('B站订阅'))
 async def set_subcribe(bot, ev: CQEvent):
     mid = util.normalize_str(ev.message.extract_plain_text())
-    if sp.check_mid(mid):
+    check_flag = await sp.check_mid(mid)
+    if check_flag:
         await switch_subscribe(bot, ev, sub=True)
 
 @sv.on_prefix(('B站退订'))
@@ -40,7 +42,11 @@ async def search_subscribe(bot, ev: CQEvent):
     await video_poller(sp, sv, send_msg=False)
     mid_list = ss.search_subscription(ev.group_id)
     msg = f'群{ev.group_id}订阅了UP主：'
+    # print(mid_list)
     for mid in mid_list:
+        # print(mid)
+        # print(_video_cache[mid].item_cache)
+        print(_video_cache[mid].item_cache[0].author)
         author = _video_cache[mid].item_cache[0].author
         msg += f'\n{author} UID：{mid}'
     await bot.send(ev, msg, at_sender=True)
@@ -55,12 +61,14 @@ def get_format(mid: str, items: List[Item]) -> str:
 
 async def video_poller(spider: BiliSpider, sv: Service, send_msg=True, interval_time=1):
     mid_list = ss.get_mids()
+    print(mid_list)
     if not _video_cache:
         for mid in mid_list:
             items_cache = ItemsCache()
             await asyncio.sleep(interval_time)
             await spider.get_update(mid, items_cache)
-            _video_cache[mid] = items_cache
+            _video_cache[mid] = copy.deepcopy(items_cache)
+            print(items_cache.item_cache)
         sv.logger.info('视频缓存为空，已加载至最新')
         return
     for mid in mid_list:
--- hoshino/modules/bilisubscribe/subscriber.py
+++ hoshino/modules/bilisubscribe/subscriber.py
@@ -14,49 +14,57 @@ except:
     import json
 
 _subscriber_config_dir = os.path.expanduser('~/.hoshino/subscriber_config/')
-os.makedirs(_service_config_dir, exist_ok=True)
+os.makedirs(_subscriber_config_dir, exist_ok=True)
 
-def _load_subscriber_config(mid):
+def _load_subscriber_config():
     config_file = os.path.join(_subscriber_config_dir, 'config.json')
     if not os.path.exists(config_file):
         return {}
     try:
         with open(config_file, encoding='utf8') as f:
             config = json.load(f)
+            config = json.loads(config)
             return config
     except Exception as e:
-        svsubscriber.logger.exception(e)
+        print(e)
         return {}
 
 def _save_subscriber_config(subscriber):
     config_file = os.path.join(_subscriber_config_dir, 'config.json')
     with open(config_file, 'w', encoding='utf8') as f:
         json.dump(
-            subscriber.config,
+            json.dumps(subscriber.config),
             f,
             ensure_ascii=False,
             indent=2
         )
 
-
 class Subscriber:
     """为订阅UP主提供分群权限管理
     """
     def __init__(self):
-        self.config: Dict[str, set()] = _load_subscriber_config(mid)
+        self.config: Dict[str, list()] = _load_subscriber_config()
 
     def add_subscription(self, mid, group_id):
-        set(self.config[mid]).add(group_id)
-        _save_subscriber_config(self)
+        if mid not in self.config:
+            print(mid)
+            self.config[mid] = list()
+        if group_id not in self.config[mid]:
+            self.config[mid].append(group_id)
+            _save_subscriber_config(self)
 
     def del_subscription(self, mid, group_id):
-        set(self.config[mid]).discard(group_id)
-        _save_subscriber_config(self)
-
+        print(self.config)
+        print(mid)
+        if mid not in self.config:
+            return
+        if group_id in self.config[mid]:
+            self.config[mid].remove(group_id)
+            _save_subscriber_config(self)
     def search_subscription(self, group_id):
         mid_list = []
-        for mid, glist in self.config:
-            if group_id in glist:
+        for mid in self.config:
+            if group_id in self.config[mid]:
                 mid_list.append(mid)
         return mid_list
 
@@ -67,7 +75,7 @@ class Subscriber:
         return self.config.keys()
 
     def get_groups(self, mid):
-        return list(config[mid])
+        return list(self.config[mid])
 
 
     
